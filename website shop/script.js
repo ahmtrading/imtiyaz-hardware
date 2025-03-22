2d115a591b1ccf01cd251b341b4bba30cae7cd04
ahmtrading <atrading689@gmail.com>
2025-03-22 15:06:42 +0530

    added index.html

---
+++ website shop/script.js
@@ -0,0 +1,57 @@
+document.getElementById("whatsapp1").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Door%20Aldrop.","_blank");
+});
+document.getElementById("whatsapp2").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Door%20Handles","_blank");
+});
+document.getElementById("whatsapp3").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Mortice%20Locks","_blank");
+});
+document.getElementById("whatsapp4").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Door%20Hinges","_blank");
+});
+document.getElementById("whatsapp5").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Door%20Closer","_blank");
+});
+document.getElementById("whatsapp6").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Tower%20Bolts","_blank");
+});
+document.getElementById("whatsapp7").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Door%20Stopper","_blank");
+});
+document.getElementById("whatsapp8").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Door%20Catcher","_blank");
+});
+document.getElementById("whatsapp9").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Doo%20Eye","_blank");
+});
+document.getElementById("whatsapp10").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Drawer%20Channels","_blank");
+});
+document.getElementById("whatsapp11").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Multi%20Locks","_blank");
+});
+document.getElementById("whatsapp12").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Cupboard%20Locks","_blank");
+});
+document.getElementById("whatsapp13").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20L-Hinges","_blank");
+});
+document.getElementById("whatsapp14").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Hydraulic%20Hinges","_blank");
+});
+document.getElementById("whatsapp15").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Profile%20Handles","_blank");
+});
+document.getElementById("whatsapp16").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Steel%20Pipes","_blank");
+});
+document.getElementById("whatsapp17").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Council%20Sockets","_blank");
+});
+document.getElementById("whatsapp18").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Designer%20Pipe%20Brackets","_blank");
+});
+document.getElementById("whatsapp19").addEventListener("click", function(){
+    window.open("https://wa.me/9831434847?text=Hello!%20I'm%20interested%20in%20your%20product!%20I%20want%20to%20buy%20Simple%20Pipe%20Brackets","_blank");
+});
